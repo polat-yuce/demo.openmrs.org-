6be2a1cf1dfb6ec0dad1a44df73d1658207f05b5
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-05-25 21:30:01 +0300

    US_401 and pom

--- src/Test/POM.java
+++ src/Test/POM.java
@@ -1,11 +1,49 @@
 package Test;
 
+import Utlity.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class POM {
 
-    public _05_PlaceOrder_Elementleri() {
+    public POM() {
         PageFactory.initElements(BaseDriver.driver, this);
-        //Buradaki elementleri Driver ili bağla
     }
+
+    @FindBy(xpath="//a[text()='Gösteri']") //driver.findElement(By
+    public WebElement demo;
+
+    @FindBy(xpath = "(//span[@class='elementor-button-content-wrapper'])[2] ")
+    public WebElement openmrs2;
+
+    @FindBy(xpath = "//span[text()='Girin OpenMRS 2 Demosu']")
+    public WebElement openmrs2demo;
+
+    @FindBy(id = "username")
+    public WebElement username;
+
+    @FindBy(id = "password")
+    public WebElement password;
+
+    @FindBy(id = "loginButton")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//span[text()='You must choose a location!']")
+    public WebElement Youmustchoosealocation;
+
+    @FindBy(id = "Inpatient Ward")
+    public WebElement InpatientWard;
+
+    @FindBy(id = "error-message")
+    public WebElement errorMassage;
+
+    @FindBy(xpath = "//a[text()='Demo']")
+    public WebElement gercekDemo;
+
+    @FindBy(css = "[class='gt_float_switcher-arrow']")
+    public WebElement language;
+
+    @FindBy(xpath = "//a[text()=' Turkish']")
+    public WebElement Turkish;
 }
---
+++ src/Test/US_401.java
@@ -0,0 +1,67 @@
+package Test;
+
+import Utlity.BaseDriver;
+import Utlity.Tools;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class US_401 extends BaseDriver {
+
+    @Test(dataProvider = "UserData", groups = {"Smoke Test", "Login"})
+    public void test(String userName, String password) {
+        POM pom = new POM();
+
+        driver.get("https://openmrs.org/");
+
+        if (driver.getCurrentUrl().equals("https://openmrs.org/tr/")) {
+            pom.demo.click();
+        } else {
+            pom.language.click();
+            pom.Turkish.click();
+            pom.demo.click();
+        }
+
+
+        js.executeScript("arguments[0].click();", pom.openmrs2);
+
+        wait.until(ExpectedConditions.elementToBeClickable(pom.openmrs2demo));
+        js.executeScript("arguments[0].click();", pom.openmrs2demo);
+
+        pom.username.sendKeys(userName);
+        pom.password.sendKeys(password);
+        pom.loginButton.click();
+        Assert.assertEquals(pom.Youmustchoosealocation.getText(), "You must choose a location!");
+
+        pom.InpatientWard.click();
+        pom.username.clear();
+        pom.password.clear();
+        pom.username.sendKeys(userName);
+        pom.password.sendKeys(password);
+        pom.loginButton.click();
+        Assert.assertEquals(pom.errorMassage.getText(), "Invalid username/password. Please try again.");
+
+    }
+
+    @DataProvider
+    Object[][] UserData() {
+        Object[][] userPass = {
+                {"Merve", "e2412sdfa"},
+                {"Ali", "fdsafsadf2"},
+                {"fdsasa", "sfsdfadafasf"},
+                {"Mehsadfasdfmet", "fadsfdassafasf"},
+                {"Mehmafsdsfet", "saafsdsffasf"},
+                {"Mehmafsdsafet", "saafsdsfdfasf"},
+        };
+        return userPass;
+    }
+
+
+}
